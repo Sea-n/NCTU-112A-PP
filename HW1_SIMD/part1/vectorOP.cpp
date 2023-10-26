9733edc7ad181c57ac5927660b59d6f6b68fd3b2
Sean Wei <me@sean.taipei>
2023-10-26 11:32:42 +0800

    HW1: finish vectorOP

--- HW1_SIMD/part1/vectorOP.cpp
+++ HW1_SIMD/part1/vectorOP.cpp
@@ -43,15 +43,40 @@ void absVector(float *values, float *output, int N)
 void clampedExpVector(float *values, int *exponents, float *output, int N)
 {
   //
-  // PP STUDENTS TODO: Implement your vectorized version of
+  // PP STUDENTS: Implement your vectorized version of
   // clampedExpSerial() here.
   //
   // Your solution should work for any value of
   // N and VECTOR_WIDTH, not just when VECTOR_WIDTH divides N
   //
-  for (int i = 0; i < N; i += VECTOR_WIDTH)
-  {
+
+  __pp_vec_float res, valF, maxF, allZeroF;
+  __pp_vec_int allOneI, allZeroI, expV;
+  __pp_mask maskAll, maskExp, maskMax;
+
+  maskAll = _pp_init_ones();  // maskAll = [* * * *]
+  _pp_vset_int(allOneI, 1, maskAll);   // allOneI = [1 1 1 1]
+  _pp_vset_int(allZeroI, 0, maskAll);  // allZeroI = [0 0 0 0]
+  _pp_vset_float(allZeroF, 0, maskAll);  // allZeroF = [.0 .0]
+  _pp_vset_float(maxF, 9.999999f, maskAll);
+
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
+    _pp_vload_int(expV, exponents + i, maskAll);  // expV = exponents
+    _pp_vload_float(valF, values + i, maskAll);   // valF = values
+    _pp_vset_float(res, 1.0, maskAll);  // res = 1.0
+
+    do {
+      _pp_vgt_int(maskExp, expV, allZeroI, maskAll);  // maskExp = (expV > 0)
+      _pp_vsub_int(expV, expV, allOneI, maskExp);  // expV -= 1
+      _pp_vmult_float(res, res, valF, maskExp);  // if (maskExp) { res *= valF }
+    } while (_pp_cntbits(maskExp));
+
+    _pp_vgt_float(maskMax, res, maxF, maskAll);  // maskMax = (res > 9.99)
+    _pp_vmove_float(res, maxF, maskMax);
+    _pp_vstore_float(output + i, res, maskAll);
   }
+
+    _pp_vstore_float(output + N, allZeroF, maskAll);
 }
 
 // returns the sum of all elements in values
@@ -61,12 +86,27 @@ float arraySumVector(float *values, int N)
 {
 
   //
-  // PP STUDENTS TODO: Implement your vectorized version of arraySumSerial here
+  // PP STUDENTS: Implement your vectorized version of arraySumSerial here
   //
 
-  for (int i = 0; i < N; i += VECTOR_WIDTH)
-  {
+  __pp_vec_float sumF, valF;
+  __pp_mask maskAll;
+
+  maskAll = _pp_init_ones();  // maskAll = [* * * *]
+  _pp_vset_float(sumF, 0.0f, maskAll);
+
+  for (int i = 0; i < N; i += VECTOR_WIDTH) {
+    _pp_vload_float(valF, values + i, maskAll);  // valF = values
+    _pp_vadd_float(sumF, sumF, valF, maskAll);
   }
 
-  return 0.0;
+  float sum[VECTOR_WIDTH];
+  for (int i=2; i<VECTOR_WIDTH; i*=2) {
+    _pp_hadd_float(sumF, sumF);  // [AB AB CD CD]
+    _pp_interleave_float(sumF, sumF);  // [AB CD AB CD]
+  }
+  _pp_hadd_float(sumF, sumF);  // [ABCD * 4]
+  _pp_vstore_float(sum, sumF, maskAll);
+
+  return sum[0];
 }
