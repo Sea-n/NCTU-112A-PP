ee92e93789a32404e0c61f66ac1ae21ef5a276b6
Sean Wei <me@sean.taipei>
2023-11-28 20:46:30 +0800

    HW4: finish part 1 task 1

--- HW4_MPI/part1/hello.cc
+++ HW4_MPI/part1/hello.cc
@@ -5,13 +5,13 @@ int main(int argc, char **argv) {
     // Initialize the MPI environment. The two arguments to MPI Init are not
     // currently used by MPI implementations, but are there in case future
     // implementations might need the arguments.
-    MPI_Init(NULL, NULL);
+    MPI_Init(&argc, &argv);
 
-    // TODO: Get the number of processes
     int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // TODO: Get the rank of the process
     int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
--- HW4_MPI/part1/pi_block_linear.cc
+++ HW4_MPI/part1/pi_block_linear.cc
@@ -14,7 +14,11 @@ int main(int argc, char **argv) {
     int world_rank, world_size;
     // ---
 
-    // TODO: init MPI
+    int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
     if (world_rank > 0) {
         // TODO: handle workers
