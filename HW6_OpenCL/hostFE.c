fd49288073206bc0119432097f9b5cb7855c4949
Sean Wei <me@sean.taipei>
2024-01-02 22:33:13 +0800

    HW6: finish

--- HW6_OpenCL/hostFE.c
+++ HW6_OpenCL/hostFE.c
@@ -8,4 +8,47 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             cl_context *context, cl_program *program) {
     cl_int status;
     int filterSize = filterWidth * filterWidth;
+
+	cl_command_queue command_queue = clCreateCommandQueue(*context, *device, 0, &status);
+	CHECK(status, "clCreateCommandQueue");
+
+
+	/* Create CL memery with host pointer */
+	cl_mem input_img_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+			imageHeight * imageWidth * sizeof(float), inputImage, &status);
+
+	cl_mem filter_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+			imageHeight * imageWidth * sizeof(float), filter, &status);
+
+	cl_mem output_img_mem = clCreateBuffer(*context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+			imageHeight * imageWidth * sizeof(float), outputImage, &status);
+
+
+	cl_kernel kernel = clCreateKernel(*program, "convolution", status);
+	CHECK(status, "clCreateKernel");
+
+	clSetKernelArg(kernel, 0, sizeof(cl_int), (void *) &filterWidth);
+	clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *) &filter_mem);
+	clSetKernelArg(kernel, 2, sizeof(cl_int), (void *) &imageHeight);
+	clSetKernelArg(kernel, 3, sizeof(cl_int), (void *) &imageWidth);
+	clSetKernelArg(kernel, 4, sizeof(cl_mem), (void *) &input_img_mem);
+	clSetKernelArg(kernel, 5, sizeof(cl_mem), (void *) &output_img_mem);
+
+	size_t global_size[2] = { imageHeight, imageWidth };
+	size_t local_size[2] = { 20, 20 };
+	status = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL,
+			global_size, local_size, 0, NULL, NULL);
+	CHECK(status, "clEnqueueNDRangeKernel");
+
+	clEnqueueMapBuffer(command_queue, output_img_mem, CL_TRUE, CL_MAP_READ,
+			0, imageHeight * imageWidth * sizeof(float), 0, 0, 0, &status);
+	CHECK(status, "clEnqueueMapBuffer");
+
+	// cleanup
+	clReleaseKernel(kernel);
+	clReleaseMemObject(input_img_mem);
+	clReleaseMemObject(filter_mem);
+	clReleaseMemObject(output_img_mem);
+	clReleaseCommandQueue(command_queue);
+	return 0;
 }
--- HW6_OpenCL/kernel.cl
+++ HW6_OpenCL/kernel.cl
@@ -1,3 +1,18 @@
-__kernel void convolution() {
+__kernel void convolution(const int filterWidth, __global float *filter,
+                          const int imageHeight, const int imageWidth,
+                          __global float *inputImage, __global float *outputImage) {
+    int hsz = filterWidth / 2;
+    int x = get_global_id(0);
+    int y = get_global_id(1);
+    float sum = 0;
+    int i, j;
 
+    for (i=-hsz; i<=hsz; i++)
+        for (j=-hsz; j<=hsz; j++)
+            if (x+i >= 0 && x+i < imageHeight &&
+                y+j >= 0 && y+j < imageWidth) {
+                sum += inputImage[(x+i) * imageWidth + (y+j)] *
+                       filter[(i+hsz) * filterWidth + (j+hsz)];
+            }
+    outputImage[x * imageWidth + y] = sum;
 }
